6491073e5ef3e3f3fd331e315bf77959e4a26da1
JulianKastaraaa09 <97487210+JulianKastaraaa09@users.noreply.github.com>
2022-01-11 08:31:59 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,20 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI DULU BANG BIAR TAMBAH CAKEP:D
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0857 4234 4873
+2. PULSA IM3
+	ㅁ 0857 4234 4873
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0857 4234 4873
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Julian Kastara
+
+*ATM*
+
+Tidak tersedia
 `
 	}
 
