4bffba62d3cd325a5193a0e4c2c247a10549fbb7
JulianKastaraaa09 <97487210+JulianKastaraaa09@users.noreply.github.com>
2022-01-13 13:35:46 +0700

    Update zeeoneofc.js

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -1,4 +1,4 @@
-const numberik = '•'
+const numberik = '│•'
 const petik = '```'
 const help = (prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensii, tekss)=> {
 	return`┌❏ *INDONESIA TIME*
